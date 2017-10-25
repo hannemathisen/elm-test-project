f2b811ea09cb329bc5704abecc203694bfc7755e
hannemathisen <hannemathisen1@gmail.com>
2017-10-25 16:02:57 +0200

    canvas shows image

--- main.elm
+++ main.elm
@@ -1,7 +1,87 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Canvas exposing (Size, Error, DrawOp(..), DrawImageParams(..), Canvas)
+import Canvas.Point exposing (Point)
+import Canvas.Point as Point
+import Canvas.Events as Events
+import Color exposing (Color)
+import Task
 
 
 main =
-  Html.text "Hello World"
+  Html.program
+    { init = ( Loading, loadImage)
+    , view = view
+    , update = update
+    , subscriptions = always Sub.none
+    }
+
+
+type Msg
+  = ImageLoaded (Result Error Canvas)
+
+
+type Model
+  = GotCanvas Canvas (List DrawOp)
+  | Loading
+
+
+loadImage : Cmd Msg
+loadImage =
+  Task.attempt
+    ImageLoaded
+    (Canvas.loadImage "./cat.jpg")
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    ImageLoaded result ->
+      case Result.toMaybe result of
+        Just canvas ->
+          ( GotCanvas canvas  []
+          , Cmd.none
+          )
+        Nothing ->
+          ( Loading
+          , loadImage
+          )
+
+
+view : Model -> Html Msg
+view model =
+  div
+    []
+    [ presentIfReady model ]
+
+
+presentIfReady : Model -> Html Msg
+presentIfReady model =
+  case model of
+    Loading ->
+      p [] [ text "loading..." ]
+    GotCanvas canvas drawOps ->
+      canvas
+        |> drawCanvas drawOps
+        |> Canvas.toHtml []
+
+
+drawCanvas : List DrawOp -> Canvas -> Canvas
+drawCanvas drawOps canvas =
+  let
+      { width, height } =
+          Canvas.getSize canvas
+
+      drawOpsWithBorder : List DrawOp
+      drawOpsWithBorder =
+          List.append
+              drawOps
+              [ BeginPath
+              , StrokeStyle (Color.rgb 255 0 0)
+              , LineWidth 2.0
+              , Rect (Point.fromInts ( 0, 0 )) (Size 200 200)
+              , Stroke
+              ]
+  in
+      Canvas.batch drawOpsWithBorder canvas
